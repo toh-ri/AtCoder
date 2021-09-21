d9732bc1b5423e8d82d8bb97e74c7c0910465e3f
tohri <thukumashikumi@gmail.com>
2021-09-22 02:41:48 +0900

    [AC] abc218 abc218_a

---
+++ atcoder.jp/abc218/abc218_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+  string S,t;
+    cin >>N>>S;
+  t="o";
+  if (S[N-1]=='o'){
+    cout<<"Yes"<<endl;
+  }else{
+  cout<<"No"<<endl;
+  }
+  
+	
+	return 0;
+}
