bed5d8d5796fa68f0e3942d04751dcd1c81ad8df
tohri <thukumashikumi@gmail.com>
2021-10-17 21:02:30 +0900

    [AC] abc223 abc223_a

---
+++ atcoder.jp/abc223/abc223_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X;
+	cin >> X;
+
+	if (X != 0 && X % 100 == 0) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+
+		return 0;
+}
