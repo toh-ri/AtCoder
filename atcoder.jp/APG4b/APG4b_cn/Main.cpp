a4b6c91ddd1ec623bb913fa31ff8eb378e1dcea9
tohri <thukumashikumi@gmail.com>
2021-10-19 20:17:36 +0900

    [AC] APG4b APG4b_cn

---
+++ atcoder.jp/APG4b/APG4b_cn/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, a, b;
+	cin >> x >> a >> b;
+
+	// 1.の出力
+	x++;
+	cout << x << endl;
+	cout << x * (a + b) << endl;
+	cout << x * (a + b) * x * (a + b) << endl;
+	cout << x * (a + b) * x * (a + b) - 1 << endl;
+	// ここにプログラムを追記
+		return 0;
+}
