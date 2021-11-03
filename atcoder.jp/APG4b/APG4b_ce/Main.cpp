101dda765655cc43074e9d96f64d78c22eafb8cc
tohri <thukumashikumi@gmail.com>
2021-11-03 10:21:48 +0900

    [AC] APG4b APG4b_ce

---
+++ atcoder.jp/APG4b/APG4b_ce/Main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+	vector<int> A(M), B(M);
+	for (int i = 0; i < M; i++) cin >> A.at(i) >> B.at(i);
+
+	vector<vector<char>> result(N, vector<char>(N,'-'));
+
+	for (int i = 0; i < M; i++) {
+		result.at(A.at(i) - 1).at(B.at(i) - 1) = 'o';
+		result.at(B.at(i) - 1).at(A.at(i) - 1) = 'x';
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << result.at(i).at(j);
+			if (j != N - 1) {
+				cout << ' ';
+			}
+			else {
+				cout << endl;
+			}
+		}
+	}
+	return(0);
+}
