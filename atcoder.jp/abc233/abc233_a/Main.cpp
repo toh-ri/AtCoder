0f00818e69f3c9b874123c08c6605c9337a7930b
tohri <thukumashikumi@gmail.com>
2021-12-25 21:03:57 +0900

    [AC] abc233 abc233_a

---
+++ atcoder.jp/abc233/abc233_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X, Y;
+	cin >> X >> Y;
+	int ans = 0;
+	if (Y - X <= 0) {
+		
+	}
+	else {
+		ans = (Y - X + 9) / 10;
+	}
+	cout << ans << endl;
+	return 0;
+}
