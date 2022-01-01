9d0d618a73e000509e6225ed66f48dbe4d3de013
tohri <thukumashikumi@gmail.com>
2022-01-01 15:26:54 +0900

    [AC] abc204 abc204_a

---
+++ atcoder.jp/abc204/abc204_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	if (x != y) {
+		cout << 3 - x - y << endl;
+	}
+	else {
+		cout << x << endl;
+	}
+		return 0;
+}
