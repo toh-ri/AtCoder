b76d4372646a83397b35d70cc17a9d192b2cc7f9
tohri <thukumashikumi@gmail.com>
2022-02-20 21:02:37 +0900

    [AC] abc240 abc240_a

---
+++ atcoder.jp/abc240/abc240_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if (abs(a - b) == 1 || abs(a - b) == 9) {
+		cout << "Yes" << endl;
+	}
+	else {
+		cout << "No" << endl;
+	}
+	return 0;
+}
